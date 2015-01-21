b8d3e1c16faa63f1a6388d87312afc49a3e777f4
堀口誠人 <horiguchi_seito@liginc.co.jp>
2015-01-21 18:04:11 +0900

    斬アニメーション追加

--- sp/dev/js/animation1.js
+++ sp/dev/js/animation1.js
@@ -2,22 +2,7 @@
 
     'use strict';
 
-  
 
-    // $(window).on('load',function(){
-    //     //読み込み完了したらloadingを非表示
-    //     $('#js_load').fadeOut(2000,
-    //     function(){
-    //         //切れ目アニメーション
-    //         $('#slash_img_inner').addClass('is_animating');
-
-    //         $('#slash_img_inner').on('webkitAnimationEnd',function(){
-    //             setTimeout(function(){
-    //                 $('#js_slash').addClass('is_animating');
-    //             },1000);
-    //         });
-    //     });
-    // });
 
     var $slash =$('#js_slash'),
         $slashInner = $slash.find('.slash_img_inner');
@@ -31,6 +16,11 @@
             setTimeout(function(){
                 $slash.addClass('is_animating');
             },1000);
+        });
+
+        //.slashを丸ごと消す
+        $slash.find('.up').on('webkitAnimationEnd',function(){
+            $slash.find('.slash_inner').addClass('is_hidden');
         });
     });
 
